c7a27bbe0fc4096a4e4f9273394fca816b39ef70
Dimitrashiky <luciandimitras@gmail.com>
2024-08-03 22:50:53 +0300

    Add files via upload

---
+++ module 7_3_3.py
@@ -0,0 +1,16 @@
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 350.4
+
+print("В команде Мастера кода участников: %(team_num)s!" % {"team_num": 6})
+print("Итого сегодня в командах участников: %(team_num1)s и %(team_num2)s!" % {"team_num1": 5, "team_num2": 6})
+print("Команда Волшебники данных решила задач: {score_2}!".format(score_2 = 42))
+print("Волшебники данных решили задачи за {team1_time} с!".format(team1_time = 18015.2))
+score_1, score_2 = 40, 42
+print(f"Команды решили {score_1} и {score_2} задач.")
+challenge_result = "Победа команды Мастера кода!"
+print(f"Результат битвы: {challenge_result}")
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!")
+
+
