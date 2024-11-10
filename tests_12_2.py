50b12fb3c9c7229e2c7292e20ee247aa4b8ba1fc
Dimitrashiky <luciandimitras@gmail.com>
2024-11-10 15:15:22 +0300

    Update tests_12_2.py

--- tests_12_2.py
+++ tests_12_2.py
@@ -4,7 +4,7 @@ from runner_and_tournament import Runner, Tournament
 class TournamentTest(TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.all_results = {}
+        cls.all_results = []
     def setUp(self):
         self.first = Runner("Усэйн", 10)
         self.second = Runner("Андрей", 9)
@@ -12,21 +12,24 @@ class TournamentTest(TestCase):
 
     def test_first_turnement(self):
         tournament = Tournament(90, self.first, self.third)
-        self.all_results[1] = tournament.start()
-        self.assertTrue(self.all_results[2][1] == "Ник")
+        result = tournament.start()
+        TournamentTest.all_results.append(result)
+        self.assertTrue(result[2] == "Ник")
     def test_seconde_turnament(self):
         tournament = Tournament(90, self.second, self.third)
-        self.all_results[2] = tournament.start()
-        self.assertTrue(self.all_results[2][2] == "Ник")
+        result = tournament.start()
+        TournamentTest.all_results.append(result)
+        self.assertTrue(result[2] == "Ник")
 
     def test_third_turnament(self):
         tournament = Tournament(90, self.first, self.second, self.third)
-        self.all_results[2] = tournament.start()
-        self.assertTrue(self.all_results[3][2] == "Ник")
+        result = tournament.start()
+        TournamentTest.all_results.append(result)
+        self.assertTrue(result[3] == "Ник")
 
     @classmethod
     def tearDownClass(cls):
-        for i, j in cls.all_results.items():
+        for i, j in enumerate(cls.all_results):
             print(f'{i+1}: {j}')
 
 
