f877d9eacf056cefe3779c82307cff5d7966d893
Dimitrashiky <luciandimitras@gmail.com>
2024-11-24 13:48:00 +0300

    Update module_13_1.py

--- module_13_1.py
+++ module_13_1.py
@@ -5,7 +5,7 @@ async def start_strongman(name, power):
     print(f"Силач{name} начал соревнования.")
     for i in range(1,6):
         print(f'Силач {name} поднял {i} шар')
-        await asyncio.sleep(10/power)
+        await asyncio.sleep(1/power)
     print(f'Силач {name} закончил соревнования.')
 
 async def start_tournament():
