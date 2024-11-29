ec676ecc3ac1e9a7d4665066e9584dc6011bd261
Dimitrashiky <luciandimitras@gmail.com>
2024-11-29 15:42:24 +0300

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL)''')
+
+#for i in range(1, 11):
+    #cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)", (f"User{i}", f"example{i}@gmail.com", f"{i}0", "1000"))
+#cursor.execute("UPDATE Users SET balance = 500 WHERE id % 2 != 0")
+#cursor.execute("DELETE FROM Users WHERE (id - 1) % 3 = 0")
+
+#cursor.execute("SELECT * FROM Users WHERE age != 60")
+
+#for user in cursor.fetchall():
+    #print(f'Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}')
+
+ #cursor.execute("DELETE FROM Users WHERE id = 6")
+
+
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+
+
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balances = cursor.fetchone()[0]
+print(all_balances / total_users)
+connection.commit()
+connection.close()
+
