5d6bea70ee409d101b39f12e8f02407a1e7f27f6
Dimitrashiky <luciandimitras@gmail.com>
2024-12-02 14:16:41 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+from aiogram.filters import CommandStart
+
+
+api = "7867274709:AAHYgWkZ4GZaYmksMvwwCv4U0iCSoiPnWmw"
+bot = Bot(token=api)
+dp = Dispatcher(storage= MemoryStorage())
+
+@dp.message(CommandStart())
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+
+@dp.message()
+async def all_messages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
